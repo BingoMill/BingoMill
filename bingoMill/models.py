b8bf69a8f90a1b391339c82eb54642252aea5fde
HeeSungUm <haojile96@gmail.com>
2020-12-07 11:49:28 +0900

    뮤지엄 수정중

--- bingoMill/models.py
+++ bingoMill/models.py
@@ -6,17 +6,8 @@ class Bingo(models.Model):
     title = models.CharField(max_length=20)
     password = models.CharField(max_length=100)
     username = models.CharField(max_length=30)
-    words = ArrayField(models.CharField(max_length=30, blank=True), size=37)
-    words = models.IntegerField()
+    words = models.CharField(max_length=2000)
+    words_size = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
-class Bingo(models.Model):
-    # title = models.CharField(max_length=20)
-    # password =  models.CharField(max_length=100)
-    # username = models.CharField(max_length=30)
-    # words = models.ArrayField(models.CharField(max_length=30, blank=True),size=37)
-    # wordSize = models.IntegerField()
-
-    def __str__(self):
-        return self.question_text
--- bingoMill/views.py
+++ bingoMill/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 from .models import Bingo
 
 
@@ -7,10 +12,16 @@ def edit(request):
 
 
 def museum(request):
-    return render(request, 'bingoMill/museum.html')
+    bingo_list = Bingo.objects.all()
+    print(list(bingo_list)[0].id)
+    # rows = json.dumps(list(bingo_list), cls=DjangoJSONEncoder)
+    context = {'bingo_list': list(bingo_list)}
+
+    return render(request, 'bingoMill/museum.html', context)
 
 
 def detail(request, bingo_id):
+    bingo = get_object_or_404(Bingo, pk=bingo_id)
     return render(request, 'bingoMill/detail.html', {"bingo_id": bingo_id})
 
 
@@ -20,10 +31,16 @@ def index(request):
     return render(request, 'bingoMill/index.html')
 
 
+@csrf_exempt
 def upload(request):
+    post_dict = request.POST.dict()
     bingo_instance = Bingo()
-    print(request.POST)
-    print("!2")
-    return
+    bingo_instance.title = post_dict.get("title")
+    bingo_instance.username = post_dict.get("username")
+    bingo_instance.password = post_dict.get("password")
+    bingo_instance.words = post_dict.get("words")
+    bingo_instance.words_size = post_dict.get("words_size")
+    bingo_instance.save()
+    return render(request, 'bingoMill/edit.html')
 
 # Create your views here.
